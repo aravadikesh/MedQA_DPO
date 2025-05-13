f2f7e2c113b909c5d9eb2ebe7692dfad0d9e02a8
ethan-harper81 <114507050+ethan-harper81@users.noreply.github.com>
2025-05-12 23:07:59 -0400

    final touches

--- dpo/med_dpo_loss.py
+++ dpo/med_dpo_loss.py
@@ -4,13 +4,10 @@ import torch.nn.functional as F
 import numpy as np
 
 class MedDPOLoss(nn.Module):
-    def __init__(self, learned = False, weights = [.4,.3,.3] ):
+    def __init__(self, learned = False, weights =[.4,.3,.3] ):
         super().__init__()
-        # are beta, gamma, alpha learned? Hyper?
-        # are their a different set of parameters for rejected and accepted?
         self.weights = nn.Parameter(data = torch.Tensor((1,1,1)), requires_grad = True) if learned else weights
-        self.weights.data.uniform_(-1, 1)
-
+        self.learned = learned
 
     def forward(self, 
                 chosen_logits: torch.Tensor,
@@ -25,17 +22,23 @@ class MedDPOLoss(nn.Module):
 
         '''
         
-        chosen_rewards = np.dot(self.weights, chosen_rewards)    
-        rejected_rewards = np.dot(self.weights, rejected_rewards)
+        weights = torch.tensor(self.weights, device=chosen_rewards.device, dtype=chosen_rewards.dtype, requires_grad=False)
+        
+        chosen_reward = (chosen_rewards * weights).sum(dim=1).to("cuda")    
+        rejected_reward = (rejected_rewards * weights).sum(dim=1).to("cuda")
 
-        log_pref_logits = torch.log(chosen_logits).mean()
-        log_reject_logits = torch.log(rejected_logits).mean()
+        chosen_log_probs = torch.log_softmax(chosen_logits, dim = -1)
+        rejected_log_probs = torch.log_softmax(rejected_logits, dim = -1)
 
-        conf = log_pref_logits - log_reject_logits
 
-        margins = torch.log(chosen_rewards) - torch.log(rejected_rewards)
-        margins /= 1 + F.sigmoid(conf)
+        chosen_conf = chosen_log_probs.logsumexp(dim=-1).mean()
+        rejected_conf = rejected_log_probs.logsumexp(dim=-1).mean()
 
-        loss = - F.logsigmoid( margins * (log_pref_logits - log_reject_logits))
+        conf = chosen_conf - rejected_conf
+      
+
+        margins = torch.log(chosen_reward) - torch.log(rejected_reward)
+        margins = margins / 1 + F.sigmoid(conf) #margins.clamp(-5.0, 5.0) allows qwen to train but due to overfitting, further training just cause the model to output nonsense
+        loss = - F.logsigmoid( margins * (chosen_conf - rejected_conf))
 
         return loss
